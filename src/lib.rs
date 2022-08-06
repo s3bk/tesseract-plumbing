ee5c740afe898a61769e8d943276672c043c64c3
Chris Couzens <ccouzens@gmail.com>
2022-08-06 21:35:25 +0100

    Add version method
    Bump to 0.7.0
    
    https://github.com/houqp/leptess/issues/46

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ mod tess_base_api;
 mod text;
 mod result_iterator;
 
+use self::tesseract_sys::TessVersion;
 pub use leptonica_plumbing;
 pub use leptonica_plumbing::leptonica_sys;
+use std::ffi::CStr;
 pub use tess_base_api::{
     TessBaseApi, TessBaseApiGetAltoTextError, TessBaseApiGetHocrTextError,
     TessBaseApiGetLstmBoxTextError, TessBaseApiGetTsvTextError, TessBaseApiGetUtf8TextError,
@@ -20,4 +22,10 @@ pub enum Level {
     Textline, // Line within a paragraph.
     Word,     // Word within a textline.
     Symbol    // Symbol/character within a word.
+
+/// Wrapper for [`Version`](https://tesseract-ocr.github.io/tessapi/5.x/a02438.html#a3785779c909fcdd77e24b340f5913e4b)
+///
+/// Returns the version identifier as a static string.
+pub fn version() -> &'static CStr {
+    unsafe { CStr::from_ptr(TessVersion()) }
 }
