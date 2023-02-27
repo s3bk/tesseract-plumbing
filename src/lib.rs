115294b36c13b8821d71c3fc4899cc32e1bb821d
Sebastian K <s3bk@protonmail.com>
2023-02-27 20:33:17 +0100

    complete merge

--- src/lib.rs
+++ src/lib.rs
@@ -14,15 +14,6 @@ pub use tess_base_api::{
 };
 pub use tesseract_sys;
 pub use text::Text;
-pub use result_iterator::{Bbox, ResultIterator, FontAttributes};
-
-pub enum Level {
-    Block,    // Block of text/image/separator line.
-    Para,     // Paragraph within a block.
-    Textline, // Line within a paragraph.
-    Word,     // Word within a textline.
-    Symbol    // Symbol/character within a word.
-}
 
 /// Wrapper for [`Version`](https://tesseract-ocr.github.io/tessapi/5.x/a02438.html#a3785779c909fcdd77e24b340f5913e4b)
 ///
